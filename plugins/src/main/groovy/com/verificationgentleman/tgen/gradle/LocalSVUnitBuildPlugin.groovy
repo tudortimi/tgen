d07fe6302948805699c755a763bb0d2966f26c1c
Tudor Timi <tudortimi@users.noreply.github.com>
2025-03-23 17:30:10 +0100

    Fix how dependency on hdvl.svunit-build plugin is specified
    It's not really clear why `implementation` works and `runtimeClasspath` doesn't, as the HDVL code isn't used during compliation.

--- plugins/src/main/groovy/com/verificationgentleman/tgen/gradle/LocalSVUnitBuildPlugin.groovy
+++ plugins/src/main/groovy/com/verificationgentleman/tgen/gradle/LocalSVUnitBuildPlugin.groovy
@@ -4,8 +4,6 @@ import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.initialization.Settings
-import org.gradle.api.internal.project.DefaultProject
-import org.gradle.internal.classpath.DefaultClassPath
 
 class LocalSVUnitBuildPlugin implements Plugin<Settings> {
 
@@ -15,29 +13,9 @@ class LocalSVUnitBuildPlugin implements Plugin<Settings> {
         settings.getGradle().rootProject(new Action<Project>() {
             @Override
             public void execute(Project project) {
-                addInjectedPluginToClassPath(project);
                 project.apply plugin: "com.verificationgentleman.gradle.hdvl.svunit-build"
             }
         });
-    }
-
-    private addInjectedPluginToClassPath(Project project) {
-        project.buildscript {
-            repositories {
-                gradlePluginPortal()
-            }
-            dependencies {
-                classpath "com.verificationgentleman.gradle:gradle-hdvl:0.2.7"
-            }
-        }
-
-        // XXX WORKAROUND Setting the runtime classpath in the build script doesn't cause Gradle to
-        // add the custom plugin to the injected build's classpath. Programmatic changes to
-        // 'project.buildscript' also don't seem to have any effect. Adding the paths directly to
-        // the class loader works.
-        DefaultProject defaultProject = (DefaultProject) project
-        defaultProject.classLoaderScope.local(DefaultClassPath.of(
-            project.buildscript.configurations.classpath.files))
     }
 
 }
